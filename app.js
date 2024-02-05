df6901e2c24f19a4ad35bab214fbfe0407bdb3a2
messex04 messex04 <messina.daniele.2004@gmail.com>
2024-02-05 10:13:34 +0000

    lavorofinito

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'pug'); //Dico a express di usare pug come motore di template
+app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file
+
+app.get('/', function (req, res) {
+//res.send('Ciao Mondo');
+res.render('index'); //Dico a express di processare e inviare la pagina index.pug
+});
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
+});
